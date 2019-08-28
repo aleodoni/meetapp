7e15d107d310d3eedce8f6a3d4acad8f6843601b
aleodoni <aleodoni@gmail.com>
2019-08-28 10:45:09 -0300

    desafio 02 finalizado

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import express from 'express';
 
 import routes from './routes';
+import './database';
 
 class App {
   constructor() {
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,22 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
+
+/**
+ * Routes without token validation
+ */
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+/**
+ * Routes with token validation
+ */
+routes.put('/users', UserController.update);
 
 export default routes;
